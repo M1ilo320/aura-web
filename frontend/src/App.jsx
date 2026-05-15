dd8529a0884947d8edbddd1c3b6d608976f20a55
M1ilo320 <okimilosz@gmail.com>
2026-05-15 02:28:59 +0200

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import ShopView from './components/ShopView';
-import './index.css';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/s/:slug" element={<ShopView />} />
-        {/* Przekierowanie jeśli ktoś wejdzie na stronę główną bez sluga */}
-        <Route path="/" element={<div style={{color: 'white', textAlign: 'center', marginTop: '50px'}}>Wpisz /s/twoj-slug w adresie</div>} />
+        <Route path="/" element={<div style={{color: 'white', padding: '50px', textAlign: 'center'}}>AuraStore SaaS - Podaj slug sklepu w URL</div>} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
